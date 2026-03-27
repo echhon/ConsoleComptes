517bcbbbc2e0725a775288bf5899f9f820ae17d6
emili <emili@GGMP>
2026-03-27 13:53:29 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleComptes/TransactionCompte.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleComptes
+{
+    public class TransactionCompte
+    {
+        public DateTime Date { get; set; }
+        public decimal Montant { get; set; }
+        public string Devise { get; set; }
+        public string Categorie { get; set; }
+
+        public TransactionCompte(DateTime date, decimal montant, string devise, string categorie)
+        {
+            Date = date;
+            Montant = montant;
+            Devise = devise;
+            Categorie = categorie;
+        }
+
+    }
+}
