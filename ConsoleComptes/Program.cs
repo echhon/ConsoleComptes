61f0c667718e009b597987ed5f8e50420b8beedb
echhon <echhon>
2026-03-27 15:08:32 +0100

    Indentation Codes ok

--- ConsoleComptes/Program.cs
+++ ConsoleComptes/Program.cs
@@ -8,42 +8,42 @@ using ConsoleComptes;
 using System.IO;
 
 namespace ConsoleComptes
-{ 
-
-class Program
 {
-    static void Main()
+
+    class Program
     {
-        var analyseurFichier = new AnalyseurCSV();                
-        var service = new ServiceCalculs();
-
-        // 1. On récupère les données
-        var donnees = analyseurFichier.LireFichier("account_20230228.csv");
-
-        // 2. On demande à l'utilisateur de saisir une date pour calculer le solde à cette date
-        Console.Write("Veuillez saisir une date au format dd/mm/yyyy pour connaître le solde à cette date : ");
-            
-        try
+        static void Main()
         {
+            var analyseurFichier = new AnalyseurCSV();
+            var service = new ServiceCalculs();
+
+            // 1. On récupère les données
+            var donnees = analyseurFichier.LireFichier("account_20230228.csv");
+
+            // 2. On demande à l'utilisateur de saisir une date pour calculer le solde à cette date
+            Console.Write("Veuillez saisir une date au format dd/mm/yyyy pour connaître le solde à cette date : ");
+
+            try
+            {
                 decimal resultat;
                 if (DateTime.TryParse(Console.ReadLine(), out DateTime dateChoisie))
                 {
                     resultat = service.CalculerSoldeADate(dateChoisie, donnees.Solde, donnees.Liste);
                     Console.WriteLine($"Solde au {dateChoisie.ToString("dd/MM/yyyy")} : {resultat.ToString()} EUR");
-                }                
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Format de date invalide. Veuillez saisir une date au format dd/MM/yyyy.");
+            }
+
+            // 3. On affiche les plus grandes catégories en débits/dépenses
+            Console.WriteLine("\nVoici les plus grandes catégories de débit :");
+            service.AfficherTopCategoriesDebit(donnees.Liste);
+
+
+
+
         }
-        catch (FormatException)
-        {
-            Console.WriteLine("Format de date invalide. Veuillez saisir une date au format dd/MM/yyyy.");
-        }
-
-        // 3. On affiche les plus grandes catégories en débits/dépenses
-        Console.WriteLine("\nVoici les plus grandes catégories de débit :");
-        service.AfficherTopCategoriesDebit(donnees.Liste);
-
-
-
-
     }
-}
 }
